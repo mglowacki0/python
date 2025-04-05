c3829960815daac3f4df1ab8bd45f45b973a8030
mglowacki0 <xxxcobraz@gmail.com>
2025-04-05 03:34:48 +0200

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,12 @@
 import csv
+import json
+import os
 
-def load_routes(filepath):
+def load_csv_data(filepath):
     with open(filepath, newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         return [row for row in reader]
 
-def load_weather(filepath):
-    with open(filepath, newline='', encoding='utf-8') as f:
-        reader = csv.DictReader(f)
-        return [row for row in reader]
+def load_json_data(filepath):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        return json.load(f)
